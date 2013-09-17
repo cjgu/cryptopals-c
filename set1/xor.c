5f79c553198cbaceed5b843dbb743fd07c4524c4
Carl Johan Gustavsson <carl.j.gustavsson@gmail.com>
2013-09-17 22:42:35 +0200

    Add xor program

---
+++ set1/xor.c
@@ -0,0 +1,46 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdio.h>
+
+
+#include "utils.h"
+
+void 
+xor(uint8_t *in1, uint8_t *in2, int len, uint8_t **out)
+{
+  *out = calloc(len, sizeof(uint8_t));
+
+  for (int i = 0; i < len; i++)
+  {
+    (*out)[i] = in1[i] ^ in2[i];
+  }
+}
+
+int
+main(int argc, char *argv[])
+{
+  if (argc != 3){
+    exit(-1);
+  }
+  uint8_t *input1;
+  int input1_len = decode_hex(argv[1], &input1);
+
+  uint8_t *input2;
+  int input2_len = decode_hex(argv[2], &input2);
+
+  if (input1_len != input2_len){
+    exit(-1);
+  }
+
+  uint8_t *output;
+  xor(input1, input2, input1_len, &output);
+
+  char *output_str = encode_hex(output, input1_len);
+
+  printf("%s\n", output_str);
+
+  return 0;
+}
